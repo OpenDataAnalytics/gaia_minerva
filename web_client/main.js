9dbc67d79ff7bafa8164245285d4d00a47b8616c
Matthew Ma <matthew.ma@kitware.com>
2017-07-31 12:21:19 -0400

    Adds the missed main.js file;

---
+++ web_client/main.js
@@ -0,0 +1,4 @@
+import analysisWidgetRegistry from '../../minerva/web_external/views/body/analysisWidgetRegistry'
+import GaiaProcessWidget from './views/gaiaProcessWidget';
+
+analysisWidgetRegistry.register('gaia_process', GaiaProcessWidget);
